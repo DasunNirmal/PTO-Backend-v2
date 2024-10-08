800b91bd675d291f10746559ac2303674d87f93c
DasunNirmal <dasun200301@gmail.com>
2024-10-08 15:17:50 +0530

    customer crud done

--- src/main/java/lk/ijse/ptobackendv2/service/CustomerService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/CustomerService.java
@@ -1,4 +1,14 @@
 package lk.ijse.ptobackendv2.service;
 
+import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDto customerDto);
+    List<CustomerDto> getAllCustomers();
+    void deleteCustomers(String customerID);
+    void updateCustomers(String customerID, CustomerDto customerDto);
+    CustomerDto getCustomersByID(String customerId);
+    CustomerDto getCustomersByPhoneNumber(String customerPhoneNumber);
 }
