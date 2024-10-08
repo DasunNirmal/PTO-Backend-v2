d95481c702037de425b8a9c342a54b74acf4b7f1
DasunNirmal <dasun200301@gmail.com>
2024-10-08 22:08:39 +0530

    save and get done

--- src/main/java/lk/ijse/ptobackendv2/controller/ItemController.java
+++ src/main/java/lk/ijse/ptobackendv2/controller/ItemController.java
@@ -1,11 +1,46 @@
 package lk.ijse.ptobackendv2.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
+import lk.ijse.ptobackendv2.dto.impl.ItemDto;
+import lk.ijse.ptobackendv2.exception.DataPersistException;
+import lk.ijse.ptobackendv2.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @RequestMapping("/api/v2/itemController")
 public class ItemController {
+    @Autowired
+    private ItemService itemService;
+    static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDto itemDto) {
+        logger.info("POST Request Received");
+        try {
+            itemService.saveItem(itemDto);
+            logger.info("Item Saved Successfully");
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            logger.error("Data persistence error: ", e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error("Internal server error: ", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ItemDto> getAllItems() {
+        logger.info("GET Request Received");
+        return itemService.loadAllItems();
+    }
 }
