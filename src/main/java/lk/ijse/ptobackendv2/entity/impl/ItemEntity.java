e7f131650a09f9e5e18ea8a92b75671748419d59
DasunNirmal <dasun200301@gmail.com>
2024-10-08 22:07:46 +0530

    save and get done

--- src/main/java/lk/ijse/ptobackendv2/entity/impl/ItemEntity.java
+++ src/main/java/lk/ijse/ptobackendv2/entity/impl/ItemEntity.java
@@ -1,6 +1,21 @@
 package lk.ijse.ptobackendv2.entity.impl;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lk.ijse.ptobackendv2.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Items")
 public class ItemEntity implements SuperEntity {
+    @Id
+    private String itemID;
+    private String itemName;
+    private double itemPrice;
+    private int itemQty;
 }
