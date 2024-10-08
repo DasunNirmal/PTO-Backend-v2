1e4475e8ac10e79b61b6a4af1ce33e74bcb4d299
DasunNirmal <dasun200301@gmail.com>
2024-10-08 15:17:03 +0530

    customer mappings added

--- src/main/java/lk/ijse/ptobackendv2/uill/Mapping.java
+++ src/main/java/lk/ijse/ptobackendv2/uill/Mapping.java
@@ -1,4 +1,27 @@
 package lk.ijse.ptobackendv2.uill;
 
+import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
+import lk.ijse.ptobackendv2.entity.impl.CustomerEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /*customer mapping*/
+    public CustomerEntity toCustomerEntity(CustomerDto customerDto) {
+        return modelMapper.map(customerDto, CustomerEntity.class);
+    }
+    public CustomerDto toCustomerDto(CustomerEntity customerEntity) {
+        return modelMapper.map(customerEntity, CustomerDto.class);
+    }
+    public List<CustomerDto> toCustomerDtoList(List<CustomerEntity> customerEntityList) {
+        return modelMapper.map(customerEntityList, new TypeToken<List<CustomerDto>>() {}.getType());
+    }
 }
