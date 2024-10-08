7c3d4f3c5a75624b400f78a01b9d7cd4d8a4d6ea
DasunNirmal <dasun200301@gmail.com>
2024-10-08 15:19:21 +0530

    customer dto updated

--- src/main/java/lk/ijse/ptobackendv2/dto/impl/CustomerDto.java
+++ src/main/java/lk/ijse/ptobackendv2/dto/impl/CustomerDto.java
@@ -1,4 +1,17 @@
 package lk.ijse.ptobackendv2.dto.impl;
 
-public class CustomerDto {
+import lk.ijse.ptobackendv2.dto.CustomerStatus;
+import lk.ijse.ptobackendv2.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerDto implements SuperDto, CustomerStatus {
+    private String customerID;
+    private String customerName;
+    private String customerAddress;
+    private String customerPhoneNumber;
 }
