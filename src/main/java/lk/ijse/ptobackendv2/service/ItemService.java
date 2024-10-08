c67c91a4dca2fabee65497261ecf4875e9566dce
DasunNirmal <dasun200301@gmail.com>
2024-10-08 22:07:29 +0530

    save and get done

--- src/main/java/lk/ijse/ptobackendv2/service/ItemService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/ItemService.java
@@ -1,4 +1,10 @@
 package lk.ijse.ptobackendv2.service;
 
+import lk.ijse.ptobackendv2.dto.impl.ItemDto;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDto itemDto);
+    List<ItemDto> loadAllItems();
 }
