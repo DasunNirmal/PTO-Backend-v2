1dbd1b8526cd0b9bc95773a62a20011ac9c82a9f
DasunNirmal <dasun200301@gmail.com>
2024-10-09 11:24:22 +0530

    update and search done

--- src/main/java/lk/ijse/ptobackendv2/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/ptobackendv2/service/impl/ItemServiceImpl.java
@@ -2,10 +2,9 @@ package lk.ijse.ptobackendv2.service.impl;
 
 import lk.ijse.ptobackendv2.dao.ItemDao;
 import lk.ijse.ptobackendv2.dto.impl.ItemDto;
-import lk.ijse.ptobackendv2.entity.impl.CustomerEntity;
 import lk.ijse.ptobackendv2.entity.impl.ItemEntity;
-import lk.ijse.ptobackendv2.exception.CustomerNotFoundException;
 import lk.ijse.ptobackendv2.exception.DataPersistException;
+import lk.ijse.ptobackendv2.exception.ItemNotFoundException;
 import lk.ijse.ptobackendv2.service.ItemService;
 import lk.ijse.ptobackendv2.uill.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +17,11 @@ import java.util.Optional;
 @Service
 @Transactional
 public class ItemServiceImpl implements ItemService {
-
     @Autowired
     private ItemDao itemDao;
+
     @Autowired
     private Mapping mapping;
-
     @Override
     public void saveItem(ItemDto itemDto) {
         ItemEntity save = itemDao.save(mapping.toItemEntity(itemDto));
@@ -38,12 +36,34 @@ public class ItemServiceImpl implements ItemService {
     }
 
     @Override
+    public ItemDto searchItems(String itemID) {
+        if (itemDao.existsById(itemID)) {
+            ItemEntity item = itemDao.getReferenceById(itemID);
+            return mapping.toItemDto(item);
+        } else {
+            throw new ItemNotFoundException("Item not found");
+        }
+    }
+
+    @Override
     public void deleteItems(String itemID) {
         Optional<ItemEntity> itemFound = itemDao.findById(itemID);
         if (itemFound.isEmpty()) {
-            throw new CustomerNotFoundException("Item not found");
+            throw new ItemNotFoundException("Item not found");
         } else {
             itemDao.deleteById(itemID);
+        }
+    }
+
+    @Override
+    public void updateItems(String itemID, ItemDto itemDto) {
+        Optional<ItemEntity> itemFound = itemDao.findById(itemID);
+        if (itemFound.isEmpty()) {
+            throw new ItemNotFoundException("Item not found");
+        } else {
+            itemFound.get().setItemName(itemDto.getItemName());
+            itemFound.get().setItemPrice(itemDto.getItemPrice());
+            itemFound.get().setItemQty(itemDto.getItemQty());
         }
     }
 }
