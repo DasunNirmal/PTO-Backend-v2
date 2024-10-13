6dc65193d54bfdb25a3ffa9421643d4780e00a3d
DasunNirmal <dasun200301@gmail.com>
2024-10-13 23:47:51 +0530

    save done

--- src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
@@ -1,4 +1,8 @@
 package lk.ijse.ptobackendv2.service;
 
+import lk.ijse.ptobackendv2.dto.impl.CombinedOrderDto;
+import lk.ijse.ptobackendv2.dto.impl.OrderDto;
+
 public interface OrderService {
+    void saveOrder(OrderDto orderDto, CombinedOrderDto combinedOrderDto);
 }
