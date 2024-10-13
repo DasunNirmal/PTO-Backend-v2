02d08d9de17e69b8b1dd168e6401fcf32092c650
DasunNirmal <dasun200301@gmail.com>
2024-10-14 02:00:54 +0530

    delete done

--- src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
@@ -1,7 +1,6 @@
 package lk.ijse.ptobackendv2.service;
 
 import lk.ijse.ptobackendv2.dto.impl.CombinedOrderDto;
-import lk.ijse.ptobackendv2.dto.impl.OrderDetailsDto;
 import lk.ijse.ptobackendv2.dto.impl.OrderDto;
 
 import java.util.List;
@@ -9,4 +8,5 @@ import java.util.List;
 public interface OrderService {
     void saveOrder(OrderDto orderDto, CombinedOrderDto combinedOrderDto);
     List<CombinedOrderDto> loadAllOrders();
+    void deleteItems(String orderID, String itemID, int orderQty);
 }
