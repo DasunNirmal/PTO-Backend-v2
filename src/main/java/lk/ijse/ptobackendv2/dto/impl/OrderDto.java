d98fe8b40f355214214959bf43fd984279a55eb2
DasunNirmal <dasun200301@gmail.com>
2024-10-13 23:50:21 +0530

    order dto created and updated

--- src/main/java/lk/ijse/ptobackendv2/dto/impl/OrderDto.java
+++ src/main/java/lk/ijse/ptobackendv2/dto/impl/OrderDto.java
@@ -1,4 +1,19 @@
 package lk.ijse.ptobackendv2.dto.impl;
 
-public class OrderDto {
+import lk.ijse.ptobackendv2.dto.OrderStatus;
+import lk.ijse.ptobackendv2.dto.SuperDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderDto implements SuperDto, OrderStatus {
+    private String orderID;
+    private String orderDate;
+    private String customerID;
+    /*private List<OrderDetailsDto> orderDetails;*/
 }
