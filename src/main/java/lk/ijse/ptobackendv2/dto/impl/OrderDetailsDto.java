7cde40e68b35f35011202ec97cf16f1d8eda4406
DasunNirmal <dasun200301@gmail.com>
2024-10-13 23:50:46 +0530

    order details dto created and updated

--- src/main/java/lk/ijse/ptobackendv2/dto/impl/OrderDetailsDto.java
+++ src/main/java/lk/ijse/ptobackendv2/dto/impl/OrderDetailsDto.java
@@ -1,5 +1,7 @@
 package lk.ijse.ptobackendv2.dto.impl;
 
+import lk.ijse.ptobackendv2.dto.OrderDetailsStatus;
+import lk.ijse.ptobackendv2.dto.SuperDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class OrderDetails {
+public class OrderDetailsDto implements SuperDto, OrderDetailsStatus {
+    private String detailsID;
+    private OrderDto orderID;
     private String itemID;
     private String itemName;
     private double itemPrice;
     private int itemQty;
     private int orderQty;
+    private String orderDate;
+    private String customerID;
     private double totalPrice;
 }
