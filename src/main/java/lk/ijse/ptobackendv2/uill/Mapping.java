8edc59ace8b2b9d2d548991a5c460c0a2e90be25
DasunNirmal <dasun200301@gmail.com>
2024-10-13 23:37:29 +0530

    mapping for order and order details

--- src/main/java/lk/ijse/ptobackendv2/uill/Mapping.java
+++ src/main/java/lk/ijse/ptobackendv2/uill/Mapping.java
@@ -1,9 +1,14 @@
 package lk.ijse.ptobackendv2.uill;
 
+import lk.ijse.ptobackendv2.dao.ItemDao;
 import lk.ijse.ptobackendv2.dto.impl.CustomerDto;
 import lk.ijse.ptobackendv2.dto.impl.ItemDto;
+import lk.ijse.ptobackendv2.dto.impl.OrderDetailsDto;
+import lk.ijse.ptobackendv2.dto.impl.OrderDto;
 import lk.ijse.ptobackendv2.entity.impl.CustomerEntity;
 import lk.ijse.ptobackendv2.entity.impl.ItemEntity;
+import lk.ijse.ptobackendv2.entity.impl.OrderDetailsEntity;
+import lk.ijse.ptobackendv2.entity.impl.OrderEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +16,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
 @Component
 public class Mapping {
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private ItemDao itemDao;
 
     /*customer mapping*/
     public CustomerEntity toCustomerEntity(CustomerDto customerDto) {
@@ -23,10 +31,10 @@ public class Mapping {
     public CustomerDto toCustomerDto(CustomerEntity customerEntity) {
         return modelMapper.map(customerEntity, CustomerDto.class);
     }
+
     public List<CustomerDto> toCustomerDtoList(List<CustomerEntity> customerEntityList) {
         return modelMapper.map(customerEntityList, new TypeToken<List<CustomerDto>>() {}.getType());
     }
-
     /*item mapping*/
     public ItemEntity toItemEntity(ItemDto itemDto) {
         return modelMapper.map(itemDto, ItemEntity.class);
@@ -34,7 +42,31 @@ public class Mapping {
     public ItemDto toItemDto(ItemEntity itemEntity) {
         return modelMapper.map(itemEntity, ItemDto.class);
     }
+
     public List<ItemDto> toItemDtoList(List<ItemEntity> itemEntityList) {
         return modelMapper.map(itemEntityList, new TypeToken<List<ItemDto>>() {}.getType());
+    }
+    /*order mapping*/
+    public OrderEntity toOrderEntity(OrderDto orderDto) {
+        return modelMapper.map(orderDto, OrderEntity.class);
+    }
+    public OrderDto toOrderDto(OrderEntity orderEntity) {
+        return modelMapper.map(orderEntity, OrderDto.class);
+    }
+
+    public List<OrderDto> toOrderDtoList(List<OrderEntity> orderEntityList) {
+        return modelMapper.map(orderEntityList, new TypeToken<List<OrderDto>>() {}.getType());
+    }
+
+    /*order details mapping*/
+    public OrderDetailsEntity toOrderDetailsEntity(OrderDetailsDto orderDetailsDto) {
+        return modelMapper.map(orderDetailsDto, OrderDetailsEntity.class);
+    }
+    public OrderDetailsDto toOrderDetailsDto(OrderDetailsEntity orderDetailsEntity) {
+        return modelMapper.map(orderDetailsEntity, OrderDetailsDto.class);
+    }
+
+    public List<OrderDetailsDto> toOrderDetailsDtoList(List<OrderDetailsEntity> orderDetailsEntities) {
+        return modelMapper.map(orderDetailsEntities, new TypeToken<List<OrderDetailsDto>>() {}.getType());
     }
 }
