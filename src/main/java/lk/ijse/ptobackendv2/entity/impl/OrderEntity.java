348244ace0f5e6f158ae1e612102277c83f62910
DasunNirmal <dasun200301@gmail.com>
2024-10-13 23:48:07 +0530

    save done

--- src/main/java/lk/ijse/ptobackendv2/entity/impl/OrderEntity.java
+++ src/main/java/lk/ijse/ptobackendv2/entity/impl/OrderEntity.java
@@ -1,4 +1,25 @@
 package lk.ijse.ptobackendv2.entity.impl;
 
-public class OrderEntity {
+import jakarta.persistence.*;
+import lk.ijse.ptobackendv2.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Orders")
+public class OrderEntity implements SuperEntity {
+    @Id
+    private String orderID;
+    private String orderDate;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "customerID",nullable = true)
+    private CustomerEntity customer;
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<OrderDetailsEntity> orderDetails;
 }
