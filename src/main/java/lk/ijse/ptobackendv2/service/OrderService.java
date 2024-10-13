50e66f5d2b442d477398bed825f611d193e2e1bb
DasunNirmal <dasun200301@gmail.com>
2024-10-14 00:38:41 +0530

    load all orders

--- src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
+++ src/main/java/lk/ijse/ptobackendv2/service/OrderService.java
@@ -1,8 +1,12 @@
 package lk.ijse.ptobackendv2.service;
 
 import lk.ijse.ptobackendv2.dto.impl.CombinedOrderDto;
+import lk.ijse.ptobackendv2.dto.impl.OrderDetailsDto;
 import lk.ijse.ptobackendv2.dto.impl.OrderDto;
+
+import java.util.List;
 
 public interface OrderService {
     void saveOrder(OrderDto orderDto, CombinedOrderDto combinedOrderDto);
+    List<CombinedOrderDto> loadAllOrders();
 }
